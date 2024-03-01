74b829c2be1591c01c210d8145879a53b7ebcf05
Balumidikondla <balumidikondla153@gmail.com>
2024-03-01 23:08:49 +0530

    again

--- cart.sh
+++ cart.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-ID=($id -u)
-
+ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
@@ -12,19 +10,76 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "script started exacuting at $TIMESTAMP" &>> $LOGFILE
 
-VALIDATION (){
+VALIDATE (){
     if [ $1 -ne 0 ]
     then 
-        echo -e "$2...$R FAILURE $N"
-      else 
-        echo -e "$2...$R SUCCESS $N"
-fi
+       echo -e "$2....$R FAILED $N"
+    else
+       echo -e "$2...$G success $N"git 
+    fi
 }
 
 if [ $ID -ne 0 ]
 then 
-    echo "$R run this script with root access $N"
-    exit 1
+   echo -e "$R ERROR :: Please run this script with root access $N"
+   exit 1
 else
-    echo "$ your are root user"
+   echo "you are root user"
 fi
+
+dnf module disable nodejs -y  &>> $LOGFILE
+
+VALIDATE $? "Disable"
+
+dnf module enable nodejs:18 -y  &>> $LOGFILE
+
+VALIDATE $? "Enable"
+
+dnf install nodejs -y  &>> $LOGFILE
+
+VALIDATE $? "INSTALLING"
+id roboshop
+if [ $? -ne 0 ]
+then 
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
+else
+   echo  -e "roboshp user already exist $Y skipping $N"
+fi
+
+
+
+mkdir -p /app
+
+VALIDATE $? "MAKE THE DIRECTORY"
+
+curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip
+
+VALIDATE $? "Download the directory"
+
+cd /app 
+
+unzip /tmp/cart.zip
+
+VALIDATE $? "unzip that condition"
+
+npm install &>> $LOGFILE
+
+VALIDATE $? "Installing this "
+
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>> $LOGFILE
+
+VALIDATE $? "GIVE ACCESS TO THE DIRECTORY"
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "RELOAD THE VALUE OF THIS"
+
+systemctl enable cart &>> $LOGFILE
+
+VALIDATE $? "ENABLE THE cart"
+
+systemctl start cart &>> $LOGFILE
+
+VALIDATE $? "START THIS ACTION NOW"
+
