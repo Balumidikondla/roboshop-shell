5cb2af6c3ce044a6ebd586a27506ea8da474d994
Balumidikondla <balumidikondla153@gmail.com>
2024-03-01 19:23:30 +0530

    again

--- user.sh
+++ user.sh
@@ -1,30 +1,91 @@
 #!/bin/bash
 
 ID=$(id -u)
-
-R="\e[31m
-G="\e[32m
-Y="\e[33m
-N="\e[0m
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "The exacution startwith $TIMESTAMP" &>> $LOGFILE
+echo "script started exacuting at $TIMESTAMP" &>> $LOGFILE
 
-VALIDATION (){
+VALIDATE (){
     if [ $1 -ne 0 ]
     then 
-        echo -e "$2...$R FAILURE $N"
+       echo -e "$2....$R FAILED $N"
     else
-        echo -e "$2...$R SUCCESS $N"
+       echo -e "$2...$G success $N"git 
     fi
 }
 
 if [ $ID -ne 0 ]
-then
-   echo -e "$R Error: please run this script with root access $N"
+then 
+   echo -e "$R ERROR :: Please run this script with root access $N"
    exit 1
 else
-   echo "you are root access"
+   echo "you are root user"
 fi
+
+dnf module disable nodejs -y  &>> $LOGFILE
+
+VALIDATE $? "Disable"
+
+dnf module enable nodejs:18 -y  &>> $LOGFILE
+
+VALIDATE $? "Enable"
+
+dnf install nodejs -y  &>> $LOGFILE
+
+VALIDATE $? "INSTALLING"
+
+id roboshop
+if [ $? -ne 0 ]
+then 
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
+else
+   echo  -e "roboshp user already exist $Y skipping $N"
+fi
+
+
+
+mkdir -p /app
+
+VALIDATE $? "MAKE THE DIRECTORY"
+
+curl -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip
+
+VALIDATE $? "Download the directory"
+
+cd /app 
+
+unzip /tmp/user.zip
+
+VALIDATE $? "unzip that condition"
+
+npm install &>> $LOGFILE
+
+VALIDATE $? "Installing this "
+
+systemctl daemon-reload
+
+VALIDATE $? "Reload daemon"
+
+systemctl enable user 
+
+VALIDATE $? "Enabel the value"
+
+systemctl start user
+
+VALIDATE $? "Start user"
+
+dnf install mongodb-org-shell -y
+
+VALIDATE $? "INSTALLING MONGODB"
+
+mongo --host mongdb.nagendrababu </app/schema/user.js
+
+VALIDATE $? " Host user value"
+
+
